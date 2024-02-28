b577acddd6504fb228e934afe15b472ca6058b27
Lamine005 <lamine.djobo@wintechnologie.app>
2024-02-28 15:05:18 -0500

    architecture
    Creation dossier Frontend et backend pour une meilleure struture

--- Frontend/app/index.jsx
+++ Frontend/app/index.jsx
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
+import _layout from './_layout'
 
 
 export default function Page() {
--- Frontend/components/Main/BottomTabsBar/BottomTabsBar.jsx
+++ Frontend/components/Main/BottomTabsBar/BottomTabsBar.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function BottomTabsBar({}) {
   return (
     <View>
-      <Text>BottomTabsBar mm</Text>
+      <Text>BottomTabsBar </Text>
     </View>
   )
 }
