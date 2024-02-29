f7e3fc977034066454be2d37fa4d5b97af518966
NelCDev <nelson.cuervo@wintechnologie.app>
2024-02-29 08:37:59 -0500

    Server.js (pool connection)

---
+++ Backend/mongoConnection.js
@@ -0,0 +1,22 @@
+// Ce fichier gère la connexion à une base de données MongoDB à l'aide de MongoDB Atlas.
+// Il exporte deux fonctions réutilisables : connectToMongo pour établir la connexion,
+// et getDb pour obtenir une référence à une base de données spécifique.
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb+srv://dbUser:dbUserPassword@ladb.opiabpu.mongodb.net/';
+const client = new MongoClient(url, { maxPoolSize: 20000 });
+
+// Connexion réutilisable
+const connectToMongo = async () => {
+  try {
+    await client.connect();
+    console.log('Connected to the database');
+    return client;
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+};
+
+const getDb = (dbName) => client.db(dbName);
+
+module.exports = { connectToMongo, getDb };
---
+++ Backend/server.js
@@ -0,0 +1,57 @@
+// Ce fichier configure et démarre un serveur Express pour une application Node.js.
+// Il utilise plusieurs middleware tels que express, mongoose, passport, express-session, flash, cors, etc.
+// La connexion à la base de données MongoDB est établie grâce à la fonction connectToMongo du fichier 'mongoConnection'.
+// Les routes de l'application sont définies dans les fichiers 'routes/payments/payments.routes' et 'routes/projects/projects.routes'.
+// Le serveur écoute sur le port défini par la variable d'environnement PORT ou sur le port 8000 par défaut.
+require('dotenv').config(); // lecteur de variables d'environnement 
+
+const express = require('express');
+// const mongoose = require('mongoose');
+// const passport = require('passport');
+// const session = require('express-session');
+// const flash = require('connect-flash');
+// const cors = require('cors'); 
+const { connectToMongo } = require('./mongoConnection');
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+connectToMongo();
+
+// // Express body parser
+// app.use(express.urlencoded({ extended: true, limit: '10mb' })); // Augmentez la limite selon vos besoins
+// app.use(express.json({ limit: '10mb' })); // Augmentez la limite selon vos besoins
+
+
+// // Express body parser
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // Configuration d'express-session
+// app.use(session({
+//     secret: 'votre_secret_session', // Remplacez par une chaîne aléatoire et sécurisée
+//     resave: true,
+//     saveUninitialized: true,
+// }));
+
+// // Utilisation de CORS middleware
+// app.use(cors());
+
+
+// // Express flash middleware
+// app.use(flash());
+
+// // Global variables for flash messages
+// app.use((req, res, next) => {
+//     res.locals.success_msg = req.flash('success_msg');
+//     res.locals.error_msg = req.flash('error_msg');
+//     res.locals.error = req.flash('error');
+//     next();
+// });
+
+// Configuration pour utiliser les fichiers statiques du dossier "uploads".
+//app.use("/Backend/uploads", express.static(path.join(__dirname, "uploads")));
+
+
+// Start the server
+app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
