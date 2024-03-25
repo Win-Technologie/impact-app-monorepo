f6f10da29750125f46bfe3aee27baec43e283898
Alialloz <alialloz94450@gmail.com>
2024-03-25 09:52:25 -0400

    Implementation de la fonction validateInsuranceFields

--- Backend/controllers/insurance/insurance.controller.js
+++ Backend/controllers/insurance/insurance.controller.js
@@ -11,3 +11,13 @@ const VEHICLES_COLLECTION = process.env.VEHICLES_COLLECTION;
 const mainDb = getDb(MAINDB);
 const insuranceCollection = mainDb.collection(INSURANCES_COLLECTION);
 const vehicleCollection = mainDb.collection(VEHICLES_COLLECTION);
+
+async function validateInsuranceFields(req) {
+    await Promise.all([
+        body('insuranceNumber').notEmpty().withMessage('Le numéro d\'assurance est requis').run(req),
+        body('insuranceCompany').notEmpty().withMessage('La compagnie d\'assurance est requise').run(req),
+        body('subscriber').notEmpty().withMessage('Le souscripteur est requis').run(req),
+        body('vehicle').notEmpty().withMessage('Le véhicule est requis').run(req)
+        // Vous pouvez ajouter plus de validations selon les champs de votre modèle d'assurance
+    ]);
+}
