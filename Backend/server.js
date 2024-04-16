cca2e7517b84a9d807e8a2a6e7a8e9206cff4152
Alialloz <alialloz94450@gmail.com>
2024-04-16 10:22:44 -0400

    Update

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ const MYCRONTIMER = process.env.CRONTIMER_EVERYHOUR;
 // Routes
 const userRoutes = require('./routes/user/user.routes');
 const vehicleRoutes = require('./routes/vehicle/vehicle.routes');
-// const insuranceRoutes = require('./routes/insurance/insurance.routes');
+const insuranceRoutes = require('./routes/insurance/insurance.routes');
 const onfidoRoutes = require('./routes/onfido/onfido.routes');
 
 const veriffRoutes = require('./routes/veriff/veriff.routes')
@@ -63,7 +63,7 @@ app.use("/Backend/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use('/api/users', userRoutes);
 app.use('/api/vehicles', vehicleRoutes);
-// app.use('/api/insurances', insuranceRoutes);
+app.use('/api/insurances', insuranceRoutes);
 app.use('/api/onfido', onfidoRoutes);
 
 app.use('/api/veriff', veriffRoutes);
